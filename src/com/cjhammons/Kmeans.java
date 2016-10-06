57f14269aac5310c6c712907a5bd84b208a9868a
cjhammons <cjh0030@auburn.edu>
2016-10-06 11:46:14 -0500

    Kmeans basically implemented, though it has bugs that keep it from producing a result. Right now it does not terminate.

--- src/com/cjhammons/Kmeans.java
+++ src/com/cjhammons/Kmeans.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Class that holds the Kmeans implementation as well as all supporting methods
@@ -15,14 +16,16 @@ import java.util.List;
  */
 public class Kmeans {
 
-    static final int NUM_CLUSTERS = 3;
-    List<Point> allPoints = new ArrayList<>();
+    private static final int NUM_CLUSTERS = 3;
+    private List<Point> allPoints = new ArrayList<>();
+    List<Cluster> clusters = new ArrayList<>();
 
     /**
      * Simple class that holds (x, y) coordinates
      */
     public class Point {
         double x, y;
+//        boolean isCentroid;
 
         /**
          * Constructor
@@ -32,7 +35,17 @@ public class Kmeans {
         public Point(double _x, double _y) {
             x = _x;
             y = _y;
+//            isCentroid = false;
+
         }
+
+//        public boolean isCentroid() {
+//            return isCentroid;
+//        }
+//
+//        public void setCentroid(boolean centroid) {
+//            isCentroid = centroid;
+//        }
 
         public String toString() {
             return "(" + x + ", " + y + ")";
@@ -45,13 +58,51 @@ public class Kmeans {
     public class Cluster {
         List<Point> pointList;
         Point centroid;
+        int cluserId;
 
         /**
          * Constructor
          * @param _pointList List of points in the cluster
          */
-        public Cluster(List<Point> _pointList) {
+        public Cluster(List<Point> _pointList, int id) {
             pointList = _pointList;
+            calculateCentroid();
+            cluserId = id;
+        }
+
+        /**
+         * Calculates the cluster's centroid.
+         * Sets the local centroid variable to the result.
+         */
+        public void calculateCentroid() {
+//            centroid.setCentroid(false);
+            double cx = 0;
+            double cy = 0;
+            for (int i = 0; i < pointList.size(); i++) {
+                Point p = pointList.get(i);
+                cx += p.x;
+                cy += p.y;
+            }
+            centroid = new Point(cx / pointList.size(), cy / pointList.size());
+
+            //had to comment this out because it was overloading intellij's console
+//            System.out.println("Cluster " + cluserId + "'s centroid is: " + centroid.toString());
+        }
+
+        public int getCluserId() {
+            return cluserId;
+        }
+
+        public void addPoint(Point p){
+            if (!pointList.contains(p)) {
+                pointList.add(p);
+            }
+        }
+
+        public void removePoint(Point p){
+            if (pointList.contains(p)) {
+                pointList.remove(p);
+            }
         }
 
         public Point getCentroid() {
@@ -88,9 +139,85 @@ public class Kmeans {
     }
 
     /**
+     * Calculates Euclidian distance between two points
+     * @param a
+     * @param b
+     * @return distance
+     */
+    double getDistance(Point a, Point b) {
+        //Distance formula in java form
+//        double xDist = Math.pow(a.x -  b.x, 2);
+//        double yDist = Math.pow(a.y - b.y, 2);
+        return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+    }
+
+    /**
      * Implementation of the kmeans algorithm
      */
     public void kmeans() {
         readFile();
+
+        //Get initial randomized cluster centroids
+        List initCentroids = new ArrayList();
+        Random rand = new Random();
+        for (int i = 0; i < NUM_CLUSTERS; i++) {
+            Point p = allPoints.get(rand.nextInt(allPoints.size()));
+
+            //Check if point is already in init list. if it is, decrement i so loop
+            //will repeat without adding anything
+            if (!initCentroids.contains(p)) {
+//                p.setCentroid(true);
+                initCentroids.add(p);
+                System.out.println(p.toString() + " is initial centroid.");
+                Cluster clust = new Cluster(new ArrayList<>(), i);
+                clust.addPoint(p);
+                clust.calculateCentroid();
+                clusters.add(clust);
+            } else {
+                i--;
+            }
+        }
+
+        int loopCount = 0;
+        while (true) {
+            System.out.println("Loop: " + loopCount++);
+            for (Point point : allPoints) {
+                //Get shortest distance to first cluster's centroid
+                double shortestDist = getDistance(point, clusters.get(0).getCentroid());
+
+                int clusterIndex = 0;
+                //Find distance to remaining clusters
+                for (int i = 1; i < NUM_CLUSTERS; i++){
+
+                    double dist = getDistance(point, clusters.get(i).getCentroid());
+                    //Check if distance is shorter than current shortest distance
+                    if (dist < shortestDist) {
+                        shortestDist = dist;
+                        if (clusters.get(clusterIndex).pointList.contains(point)) {
+                            clusters.get(clusterIndex).removePoint(point);
+                        }
+                        clusterIndex = i;
+                    }
+                }
+                //Add point to it's closest centroid's cluster
+                clusters.get(clusterIndex).addPoint(point);
+            }
+
+            //Calculate the new centroids for all the clusters.
+            boolean convergence = true;
+            for (int i = 0; i < clusters.size(); i++) {
+                Point curCentroid = clusters.get(i).getCentroid();
+                clusters.get(i).calculateCentroid();
+                //If centroid changed, we do not have convergence
+                if (curCentroid != clusters.get(i).getCentroid()){
+                    convergence = false;
+                }
+            }
+            //If centroid did not change for all clusters, we have achieved convergence
+            //and can end the algorithm.
+            if (convergence) {
+                return;
+            }
+        }
     }
 }
--- src/com/cjhammons/Main.java
+++ src/com/cjhammons/Main.java
@@ -1,20 +1,10 @@
 package com.cjhammons;
 
-import java.awt.Color;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-import javax.swing.JFrame;
-import de.erichseifert.gral.data.DataTable;
-import de.erichseifert.gral.plots.XYPlot;
-import de.erichseifert.gral.plots.lines.DefaultLineRenderer2D;
-import de.erichseifert.gral.plots.lines.LineRenderer;
-import de.erichseifert.gral.plots.points.PointRenderer;
-import de.erichseifert.gral.ui.InteractivePanel;
+
 
 /**
+ * Driver for kmeans.
+ *
  * Author: Curtis Hammons
  */
 public class Main {
